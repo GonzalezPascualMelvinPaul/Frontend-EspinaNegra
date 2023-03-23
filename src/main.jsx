ae751ec1ae69aab820aa57535132c9d0d28c4705
GonzalezPascualMelvinPaul <melvindragneel1@gmail.com>
2023-03-23 17:18:14 -0600

    Reparando rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import { EspinaNegraApp } from "./EspinaNegraApp";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <EspinaNegraApp />
+    <HashRouter>
+      <EspinaNegraApp />
+    </HashRouter>
   </React.StrictMode>
 );
